277be047afe00dba55c3c70f6a991219169ef36b
Anton Kotov <kotov-anton@yandex.ru>
2017-01-28 22:44:59 +0300

    ex1: computeCost is done

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -13,9 +13,9 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+predictions = X * theta;
+sqerrors = (predictions - y).^2;
+J = sum(sqerrors) / (2 * m);
 
 % =========================================================================
 
